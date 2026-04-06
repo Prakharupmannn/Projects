b8247b28ece28928d7c374091c2e860d478d49ec
Prakharupmannn <upmanjiprakhar@gmail.com>
2026-04-06 13:05:39 +0530

    "code change"

--- 4thproject/src/App.jsx
+++ 4thproject/src/App.jsx
@@ -1,5 +1,5 @@
 
-// import { BrowserRouter , Route , Routes } from 'react-router-dom'
+import { BrowserRouter , Route , Routes } from 'react-router-dom'
 // import Navbar from './components/Navbar'
 // import Home from './pages/Home'
 // import Aboutus from './pages/Aboutus'
--- 4thproject/src/pages/Login.jsx
+++ 4thproject/src/pages/Login.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from 'react'
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Login = () => {
 
     const [name, setName] = useState("")
     const [password, setPassword] = useState("")
+    const [role,setRole] = useState("") // radio state
     
     const handleSubmit = (e) => {
         e.preventDefault();
 
         console.log("name: " , name);
         console.log("password: ", password);
-    }
+        console.log("role: ",role);
 
-    
+        if(role === 'admin') {
+            Navigate("/admin")
+        } 
+        else if(role === 'user') {
+            Navigate("/user")
+        }
+        else{
+            alert("Please select a role")
+        }
+    }
 
   return (
     <div>
@@ -23,6 +34,12 @@ const Login = () => {
 
             <div className='border w-70 mt-3'>
                 Password: {" "}<input type="text" placeholder='Enter your password' value={password} onChange={(e)=> setPassword(e.target.value)}  required />
+            </div>
+
+            <div className='mt-3'>
+                Admin:<input type="radio" name='role' value="admin" checked={role === "admin"} onChange={(e) => setRole(e.target.value)}/>
+
+                User: <input type='radio' name='role' value="user" checked={role === "user"} onChange={(e)=> setRole(e.target.value)}/>
             </div>
 
             <button type='submit' className='bg-amber-500 rounded p-1 mt-3' >Login</button>
